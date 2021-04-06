b3133947285a454273827d4727ad5ab7ba3ce5a6
Jacky Nguyen <nktpro@gmail.com>
2021-04-06 15:01:33 -0400

    Cache deno

--- ci.sh
+++ ci.sh
@@ -1,6 +1,29 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+ci_build_in_shell() {
+  local DENO_DIR=${DENO_DIR:?"DENO_DIR env variable is required"}
+  local SHELL_NAME=${SHELL_NAME:?"SHELL_NAME env variable is required"}
+  local GITHUB_WORKSPACE=${GITHUB_WORKSPACE:?"GITHUB_WORKSPACE env variable is required"}
+
+  cat <<EOF | docker run \
+    --workdir /repo \
+    -i \
+    --rm \
+    -v "${GITHUB_WORKSPACE}:/repo" \
+    -v "${DENO_DIR}:/root/.cache/deno" \
+    -e "DENO_DIR=/root/.cache/deno" \
+    "${SHELL_NAME}" \
+    bash -l
+set -euo pipefail
+
+./cli.sh code_quality
+./cli.sh compile
+EOF
+
+}
+
+
 code_quality() {
   echo "Checking formatting..."
   deno fmt --unstable --check
